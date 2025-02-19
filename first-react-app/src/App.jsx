e5f52954bd5891c511314ed625dc0c6762b9881f
Karlo <lekxka@gmail.com>
2025-02-19 16:52:09 +0100

    learn props

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,7 +1,13 @@
+import User from "./components/User.jsx";
+
 function App (){
     return (
         <>
-
+            <User img=''
+                  name='Karlito'
+                  age={22}
+                  isMarried={false}
+                  hobbies={['Coding', '3D printing']} />
         </>
     );
 }
