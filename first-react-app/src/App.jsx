9141191403c7c90b8a8a4c4daa621264c316a0bd
Karlo <lekxka@gmail.com>
2025-02-19 16:41:57 +0100

    cleaning

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,21 +1,7 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import MainContent from "./components/MainContent";
-import Greeting from "./components/Greeting.jsx";
-import ProductionInfo from "./components/ProductionInfo.jsx";
-import List from "./components/List.jsx";
-import UserList from "./components/UserList.jsx";
-
 function App (){
     return (
         <>
-            <Header/>
-            <MainContent/>
-            <Greeting/>
-            <ProductionInfo/>
-            <List/>
-            <UserList/>
-            <Footer/>
+
         </>
     );
 }
