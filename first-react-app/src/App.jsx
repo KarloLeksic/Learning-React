bd53eaa6c7dcf87218a1a53603cced317742e792
Karlo <lekxka@gmail.com>
2025-02-20 17:43:11 +0100

    cleaning

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,12 +1,7 @@
-import Card from "./components/Card.jsx";
-
 function App (){
     return (
         <>
-            <Card>
-                <h1>Card</h1>
-                <p>This is some content from cart</p>
-            </Card>
+
         </>
     );
 }
