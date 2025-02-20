cefbdc669b309b96eeb1dc42f18e3d86762392a8
Karlo <lekxka@gmail.com>
2025-02-20 18:19:58 +0100

    use external icons

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,11 +1,23 @@
-import './index.css'
+import { FaCartArrowDown } from "react-icons/fa";
+import { FaComputer } from "react-icons/fa6";
+
+// Ovo bude u njihovoj dokumentaciji
+
 
 function App (){
     return (
         <>
-            <h1>Separate file for styling</h1>
+            <h1>Icons</h1>
+            <FaCartArrowDown />
+            <FaComputer />
         </>
     );
 }
 
 export default App;
+
+
+// Za instaliravanje ikona koristili smo react ikone
+// Instalirali som ih sa njihovom komandom npm install react-icons
+
+// treba ih importati
