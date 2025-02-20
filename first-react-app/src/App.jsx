282aeba60792b6fe9769168cc9046e608b8e28a7
Karlo <lekxka@gmail.com>
2025-02-20 18:04:19 +0100

    create weather component - conditional challenge

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,23 +1,27 @@
 function App (){
     return (
         <section>
-            <Password isValid={true}/>
+            <Weather temperature={26}/>
         </section>
     );
 }
 
 export default App;
 
-const ValidPassword = () => <h1>Valid Password</h1>;
-const InvalidPassword = () => <h1>Invalid Password</h1>;
+const Weather = ({ temperature }) => {
+    let weather = '';
+    
+    if(temperature < 15) {
+        weather = 'It\'s cold outside';
+    } else if (temperature < 25) {
+        weather = 'It\'s nice outside';
+    } else {
+        weather = 'It\'s hot outside';
+    }
 
-const Password = ({ isValid }) => {
-    // if(isValid){
-    //     return <ValidPassword/>;
-    // }
-    //
-    // return <InvalidPassword/>;
-
-    return isValid ? <ValidPassword/> : <InvalidPassword/>;
-//     Cak se moze i izbaciti return i {} isto bi radilo
-};
+    return (
+        <>
+            { weather }
+        </>
+    );
+}
