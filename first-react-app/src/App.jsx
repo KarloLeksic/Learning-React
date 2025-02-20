32440297f5b50b9295eb95b0fa45e72b76a9d9e4
Karlo <lekxka@gmail.com>
2025-02-20 18:14:49 +0100

    style in separate file

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,27 +1,11 @@
+import './index.css'
+
 function App (){
     return (
-        <section>
-            <InlineStyle/>
-        </section>
+        <>
+            <h1>Separate file for styling</h1>
+        </>
     );
 }
 
 export default App;
-
-const InlineStyle = ({ temperature }) => {
-    // Moze ovako ko objekt i onda se samo preda varijabla dole
-    const styles = {
-        color: 'red',
-        backgroundColor: 'lime',
-        padding: '2rem',
-    }
-
-    return (
-        <>
-            {/*Vazno je napomenuti da treba 2 puta zagrade i da se propertiji pisu ko u javascriptu zato kad to je JS*/}
-            <h1 style={styles}>
-                Inline Styling
-            </h1>
-        </>
-    );
-}
