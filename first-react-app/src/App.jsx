f56f21b19800abaac951b1fb80d2c38d3fe1cb90
Karlo <lekxka@gmail.com>
2025-02-20 17:31:05 +0100

    use closing tag, render child elements in conponent

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,17 +1,12 @@
-import User from "./components/User.jsx";
-import Person from "./components/Person.jsx";
+import Card from "./components/Card.jsx";
 
 function App (){
     return (
         <>
-            <User img='neki src da ne baca gresku'
-                  name='Karlito'
-                  age={22}
-                  isMarried={false}
-                  hobbies={['Coding', '3D printing']} />
-
-            <Person name="Karlito"
-                    age={24}/>
+            <Card>
+                <h1>Card</h1>
+                <p>This is some content from cart</p>
+            </Card>
         </>
     );
 }
---
+++ first-react-app/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card(props)  {
+    return (
+        <>
+            {props.children}
+            {/* Ovako mozemo pozvati ono sto je u komponenti kada koristimo tagove za zatvaranje */}
+        </>
+    );
+}
+
+export default Card;
