598938ae65c1dbcddb80655c0f0d4a72d1c3e3bd
Karlo <lekxka@gmail.com>
2025-02-20 18:51:39 +0100

    use useState with array of content

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,23 +1,29 @@
 import { useState } from 'react';
 
 function App (){
-    // ovo je konvencija za ovo da ide set i to nesta
-    // use state nam vrace varijablu kojoj zelimo pratiti stanje i funkciju i onda nam je najjednostavnije koristiti destructoring operator za koju varijablu zelimo pratit stanje i koja funkcija mjenja to stanje
-    // 0 je pocetno stanje, moze biti i string i bilo sto
-    const [count, setCount] = useState(0);
+    const [friends, setFriends] = useState(['Alex', 'John', 'Doe']);
 
-    // set count u sebi ima count jer smo ovako gore definirali i sad radimo sta ocemo s njim
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount(count - 1);
+    // Ovako mozemo dodati novog prijatela
+    // Ove tri tocke samo kopiraju sve sta je trenutno i naravno ovo na kraj samo dodaje
+    const addOneFriend = () => setFriends([...friends, 'Karlito']);
 
-    // Koristi se setCount funkcija i onda on zna da ce refreshati content da ga vidimo.
-    // Ako probamo samo count mijenjati onda se nece nista dogadati kad kliknemo, tj. necemo vidjeti. NE RADITI TO BEZ SET METODE
+    // Ovo ce obrisati samo johna
+    const removeOneFriend = () => setFriends(friends.filter(f => f !== 'John'));
+
+    // Promijena specificnog imena, ako nije onda vrati samo staro
+    const updateOneFriend = () => {
+        setFriends(friends.map(f => f === 'Alex' ? 'Alex Smith' : f));
+    };
 
     return (
         <>
-            <h1>{ count }</h1>
-            <button onClick={increment}>+</button>
-            <button onClick={decrement}>-</button>
+            {friends.map(f => (
+                <li key={Math.random()}>{f}</li>
+            ))}
+
+            <button onClick={addOneFriend}>Add New Friend</button>
+            <button onClick={removeOneFriend}>Remove Friend</button>
+            <button onClick={updateOneFriend}>Update Friend</button>
         </>
     );
 }
