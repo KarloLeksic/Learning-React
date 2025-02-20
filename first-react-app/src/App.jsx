aa6a9aa61eebfe225d79aa3ff5be55067f62cbf3
Karlo <lekxka@gmail.com>
2025-02-20 17:50:11 +0100

    conditional rendering

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,9 +1,23 @@
 function App (){
     return (
-        <>
-
-        </>
+        <section>
+            <Password isValid={true}/>
+        </section>
     );
 }
 
 export default App;
+
+const ValidPassword = () => <h1>Valid Password</h1>;
+const InvalidPassword = () => <h1>Invalid Password</h1>;
+
+const Password = ({ isValid }) => {
+    // if(isValid){
+    //     return <ValidPassword/>;
+    // }
+    //
+    // return <InvalidPassword/>;
+
+    return isValid ? <ValidPassword/> : <InvalidPassword/>;
+//     Cak se moze i izbaciti return i {} isto bi radilo
+};
