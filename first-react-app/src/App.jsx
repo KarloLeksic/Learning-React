b52039127bcca2268bd8e8815370ed6a76b43487
Karlo <lekxka@gmail.com>
2025-02-20 18:11:56 +0100

    inline styling with object

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -9,11 +9,17 @@ function App (){
 export default App;
 
 const InlineStyle = ({ temperature }) => {
+    // Moze ovako ko objekt i onda se samo preda varijabla dole
+    const styles = {
+        color: 'red',
+        backgroundColor: 'lime',
+        padding: '2rem',
+    }
 
     return (
         <>
             {/*Vazno je napomenuti da treba 2 puta zagrade i da se propertiji pisu ko u javascriptu zato kad to je JS*/}
-            <h1 style={{ color: 'red', backgroundColor: 'lime'}}>
+            <h1 style={styles}>
                 Inline Styling
             </h1>
         </>
