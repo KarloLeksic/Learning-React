9a5f13032ad6d4a42d30cf666b294de1e06d92c6
Karlo <lekxka@gmail.com>
2025-02-21 21:17:33 +0100

    cleaning

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,24 +1,8 @@
-// context API nam sluzi kad zelimo neke propse dijeliti u vise komponenti da ih nemoramo predavati svakoj komponenti ispod
-import { createContext } from 'react';
-import ComponentA from "./components/ComponentA.jsx";
-
-// exportamo i kreiramo Data varijablu koja ce nam glumiti podatke koji su 'globalni
-export const Data = createContext();
-export const Data1 = createContext();
-
 function App (){
-    // Ovo su nasi podaci
-    const name = 'Karlito';
-    const age = 24;
 
     return (
         <>
-            {/*Svi unutra ceju moci koristiti ove podatke - naglasavamo da je provider*/}
-            <Data.Provider value={name}>
-                <Data1.Provider value={age}>
-                    <ComponentA />
-                </Data1.Provider>
-            </Data.Provider>
+
         </>
     );
 }
