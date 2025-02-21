354535dde9a8a8bf5ceaa2cd8758c3d5c17efce9
Karlo <lekxka@gmail.com>
2025-02-21 20:45:57 +0100

    using useEffect to fetch data from API

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,10 +1,28 @@
 import { useState, useEffect } from 'react';
 
 function App (){
-    
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function getData() {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+            const data = await response.json();
+
+            if (data && data.length) setData(data);
+        }
+
+        getData();
+
+        // Prazan dependency array znaci da ce se ovo pokrenuti SAMO JEDNOM
+    }, []);
+
     return (
         <>
-
+            <ul>
+                {data.map((item) => (
+                    <li key={item.id}>{item.title}</li>
+                ))}
+            </ul>
         </>
     );
 }
