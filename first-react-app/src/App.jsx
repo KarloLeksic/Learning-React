7b87a3e267ead2e3b456d635802e7cbf2a46a4a9
Karlo <lekxka@gmail.com>
2025-02-21 20:47:38 +0100

    cleaning

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,28 +1,8 @@
-import { useState, useEffect } from 'react';
-
 function App (){
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        async function getData() {
-            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-            const data = await response.json();
-
-            if (data && data.length) setData(data);
-        }
-
-        getData();
-
-        // Prazan dependency array znaci da ce se ovo pokrenuti SAMO JEDNOM
-    }, []);
 
     return (
         <>
-            <ul>
-                {data.map((item) => (
-                    <li key={item.id}>{item.title}</li>
-                ))}
-            </ul>
+
         </>
     );
 }
