e89d8827c0a20daff95e2b9d3a0737e9c1047d43
Karlo <lekxka@gmail.com>
2025-02-22 12:53:11 +0100

    cleaning

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,24 +1,5 @@
-import { useRef } from 'react';
-
 function App() {
-    // const element = useRef(null);
-    // console.log(element); // dobijemo objekt koji ima current: null - jer smo null postavili
-
-    const inputElement = useRef(null);
-    console.log(inputElement);
-
-    const focusInput = () => {
-        inputElement.current.focus();
-        // Radimo bilo sta
-        inputElement.current.value = 'Karlito';
-    };
-
-    return (
-        <>
-            <input type="text" ref={inputElement} />
-            <button onClick={() => focusInput()}>Focus</button>
-        </>
-    );
+    return <></>;
 }
 
 export default App;
