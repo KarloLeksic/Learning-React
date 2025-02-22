0e532bc07d4986b64b1bc821eddc47ed4769a276
Karlo <lekxka@gmail.com>
2025-02-22 16:13:29 +0100

    toggle bg color project

---
+++ simple-project-src/05 - Toggle Background/App.jsx
@@ -0,0 +1,10 @@
+import ToggleBackgroundColor from './components/ToggleBackgroundColor.jsx';
+
+const App = () => {
+    return (
+        <>
+            <ToggleBackgroundColor />
+        </>
+    );
+};
+export default App;
