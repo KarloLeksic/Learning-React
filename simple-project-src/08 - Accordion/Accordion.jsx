e5f2cbdffe85609ac5e6a9567d748da1501f1149
Karlo <lekxka@gmail.com>
2025-02-22 20:19:32 +0100

    accordion project

---
+++ simple-project-src/08 - Accordion/Accordion.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import '../style.css';
+
+const Accordion = ({ title, content }) => {
+    const [isActive, setActive] = useState(false);
+
+    return (
+        <section className="accordion-card" key={title}>
+            <div className="header" onClick={() => setActive(!isActive)}>
+                <div>{title}</div>
+                <p className="icon">{isActive ? '-' : '+'}</p>
+            </div>
+
+            <div className="content">
+                {isActive && <p className="card-info">{content}</p>}
+            </div>
+        </section>
+    );
+};
+export default Accordion;
---
+++ simple-project-src/08 - Accordion/App.jsx
@@ -0,0 +1,15 @@
+import { accordionData } from './utils/content.js';
+import Accordion from './components/Accordion.jsx';
+
+const App = () => {
+    return (
+        <>
+            <div className="accordion">
+                {accordionData.map(({ title, content }) => (
+                    <Accordion title={title} content={content} />
+                ))}
+            </div>
+        </>
+    );
+};
+export default App;
