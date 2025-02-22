d7a5a0eb45c9af2fdb39e2851a7c7be99cd7a391
Karlo <lekxka@gmail.com>
2025-02-22 12:49:59 +0100

    useRef hook

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,5 +1,24 @@
+import { useRef } from 'react';
+
 function App() {
-    return <></>;
+    // const element = useRef(null);
+    // console.log(element); // dobijemo objekt koji ima current: null - jer smo null postavili
+
+    const inputElement = useRef(null);
+    console.log(inputElement);
+
+    const focusInput = () => {
+        inputElement.current.focus();
+        // Radimo bilo sta
+        inputElement.current.value = 'Karlito';
+    };
+
+    return (
+        <>
+            <input type="text" ref={inputElement} />
+            <button onClick={() => focusInput()}>Focus</button>
+        </>
+    );
 }
 
 export default App;
