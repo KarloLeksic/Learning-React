7c64d426b35bcc9487953899956a4e49a766c609
Karlo <lekxka@gmail.com>
2025-02-22 14:32:04 +0100

    fetch food project - axios

---
+++ simple-project-src/03 - Fetch Food/App.jsx
@@ -0,0 +1,13 @@
+import Main from './components/Main.jsx';
+
+// koristi se axios library
+// npm install axios i moze se importati
+
+const App = () => {
+    return (
+        <>
+            <Main />
+        </>
+    );
+};
+export default App;
