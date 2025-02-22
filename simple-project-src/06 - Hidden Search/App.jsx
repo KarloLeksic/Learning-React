084c5585f5a1b2ab5d31dc34a5120999e31729b8
Karlo <lekxka@gmail.com>
2025-02-22 16:23:42 +0100

    hidden search project

---
+++ simple-project-src/06 - Hidden Search/App.jsx
@@ -0,0 +1,10 @@
+import HiddenSearchBar from './components/HiddenSearchBar.jsx';
+
+const App = () => {
+    return (
+        <>
+            <HiddenSearchBar />
+        </>
+    );
+};
+export default App;
